74f7c5d19de463b005dd95aabe1efa087af017e4
ParmaSeanCheese <51465840+ParmaSeanCheese@users.noreply.github.com>
2023-07-06 13:48:16 -0500

    Create Week2_Challenge.js

---
+++ Week2_Challenge.js
@@ -0,0 +1,8 @@
+function FirstReverse(str) { 
+  str = ((str.split("")).reverse()).join("");
+  // code goes here  
+  return str; 
+}
+   
+// keep this function call here 
+console.log(FirstReverse(readline()));
